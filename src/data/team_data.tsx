bac48567854fd83c35152c2d9efacb71619d588e
aasif-create <aasifmmd12345@gmail.com>
2025-09-14 16:07:17 +0530

    UPDATED

--- src/data/team_data.tsx
+++ src/data/team_data.tsx
@@ -206,7 +206,7 @@ export const teamMembers: TeamMember[] = [
       role:"Member",
       department:"Computer Science",
       year:"III",
-      image: "https://res.cloudinary.com/devn2ez7p/image/upload/v1757828894/20250327_223412_edqcun.jpg",
+      image: "https://res.cloudinary.com/devn2ez7p/image/upload/v1757828752/20250327_223412_y4ylwj.jpg",
       linkedin: "https://www.linkedin.com/in/mohammed-aasif-6a36062a5",
 
   },
